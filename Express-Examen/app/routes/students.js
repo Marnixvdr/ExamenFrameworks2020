799001b193fb6e80d1ba86ea27c1b71ea53560a8
Marnixvdr <32701626+Marnixvdr@users.noreply.github.com>
2020-01-20 09:00:07 +0100

    Commit 2
    Methoden voor toevoegen en bekijken studenten in database

---
+++ Express-Examen/app/routes/students.js
@@ -0,0 +1,36 @@
+var express = require('express');
+var router = express.Router();
+var fs = require('fs');
+
+var MongoClient = require('mongodb').MongoClient
+var url = "mongodb://localhost:27017/";
+
+MongoClient.connect(url, (err, database) => {
+    if (err) return console.log(err)
+    db = database.db('exam')
+})
+
+router.get('/', (req, res) => {
+    db.collection('students').find().toArray((err, result) => {
+        if (err) return
+        res.render('list.ejs', { products: result})
+    })
+});
+
+router.get('/search', (req,res) => {
+    res.render('search.ejs',{})
+})
+
+router.post('/add', (req,res) => {
+    db.collection('students').insertOne(req.body, (err,result) => {
+        if(err) return
+        res.redirect('/')
+    })
+})
+
+router.get('/add', (req,res) => {
+    res.render('add.ejs',{})
+})
+
+module.exports = router; 
+
